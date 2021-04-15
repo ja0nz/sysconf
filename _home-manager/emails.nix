8997e8dd919ae10002d990c4de55065171f13630
ja0nz <mail@ja.nz>
2021-04-15 15:07:11 +0200

    Added email functionality

--- _home-manager/emails.nix
+++ _home-manager/emails.nix
@@ -1,14 +1,15 @@
-{ config, ... }:
-
-{
+{ lib, config, pkgs, ... }:
+let
+  mailjanz = "mail@ja.nz";
+in {
   accounts.email.accounts = {
     "mail@ja.nz" = rec {
       primary = true;
-      address = "mail@ja.nz";
-      userName = address;
+      address = mailjanz;
+      userName = mailjanz;
       realName = "Ja0nz";
       passwordCommand =
-        "gpg2 -q --for-your-eyes-only --no-tty -d ${config._secret}/mbsyncpass.gpg";
+        "gpg2 -q --for-your-eyes-only --no-tty -d ${config._secret}/mbsyncpass_maildir.gpg";
       mbsync = {
         enable = true;
         create = "maildir";
@@ -19,10 +20,22 @@
         host = "imap.purelymail.com";
         tls.enable = true;
       };
-      #      notmuch.enable = true;
     };
   };
 
-  programs.mbsync.enable = true;
-  #  programs.notmuch.enable = true;
+  home.file.".emacs.d/authinfo.gpg".source = "${config._secret}/authinfo_emacs.gpg";
+
+  programs.mbsync = {
+    enable = true;
+    package = pkgs.isync;
+  };
+  home.packages = with pkgs; [
+    mu # A collection of utilties for indexing and searching Maildirs
+  ];
+  home.activation.muInit = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    mu info | grep ${mailjanz} || mu init --my-address=${mailjanz}
+    # For multiple users;
+    # mu info | grep ${mailjanz} && mu info | grep another@email \
+    #   || mu init --my-address=${mailjanz} --my-address=another@email
+  '';
 }
