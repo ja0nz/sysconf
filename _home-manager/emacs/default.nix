001897eaf4a6deab76bd045694b436f4809eb5af
ja0nz <mail@ja.nz>
2021-04-22 17:00:36 +0200

    Moved emacs config to org mode

--- _home-manager/emacs/default.nix
+++ _home-manager/emacs/default.nix
@@ -7,11 +7,14 @@
   };
   services.emacs = { enable = true; };
 
-  /* Using Prelude Emacs with Org-Mode tangeling
-     config.el.org builds towards ~/.emacs/personal/config.el
+  /* config_packages.org -> ~/.doom.d/config.el & ~/.doom.d/packages.el
+     init.el -> ~/.doom.d/init.el
+     snippets -> ~/.doom.d/snippets
   */
   home.activation.initEmacs = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    #emacs --batch --eval "(require 'org)" \
-    #  --eval '(org-babel-tangle-file "${./config.el.org}")'
+    emacs --batch --eval "(require 'org)" --eval '(progn
+      (org-babel-tangle-file "${./config_packages.org}")
+      (org-babel-tangle-file "${./init.org}")
+      )'
   '';
 }
