25101757eeae73f06d3f72ff3bce7ae4e7cf139b
ja0nz <mail@ja.nz>
2021-05-12 11:38:20 +0200

    Moved cachix to folder

---
+++ nixOS/_shared/cachix/tttool.cachix.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://tttool.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "tttool.cachix.org-1:e/5HpIa6ZqwatH07kmO7di1p9K+AMrgkNHl/OGUUMzU="
+    ];
+  };
+}
+    
