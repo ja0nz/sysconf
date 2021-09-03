d6365c495160d537862e6bc9d66db1aef4b66fc0
ja0nz <mail@ja.nz>
2021-09-03 15:58:14 +0200

    Added home manager config to users definition

--- nixOS/_shared/users.nix
+++ nixOS/_shared/users.nix
@@ -1,13 +1,22 @@
 /* #+TITLE: Users
-   #+FILETAGS: :sys:
+   Defines users und home environment
 
    * Optional configuration
     Generate a hashedPassword:
     ~mkpasswd -m sha-512~
 */
-{ config, pkgs, ... }:
+{ lib, pkgs, config, ... }:
 
-{
+let
+  inherit (config) _configInUse _repoRoot _monoFont;
+  packagePath = _configInUse + /packages.nix;
+  overlayPath = _configInUse + /overlays.nix;
+  mainUser = "me";
+in {
+  imports = [ <home-manager/nixos> ];
+
+  services = { getty.autologinUser = mainUser; };
+
   # Disable useradd/groupadd
   users.mutableUsers = false;
   users.users = {
@@ -19,11 +28,39 @@
     me = {
       shell = pkgs.fish;
       isNormalUser = true;
-      home = "/home/me";
+      home = "/home/${mainUser}";
       extraGroups = [ "wheel" "networkmanager" "video" "audio" "libvirtd" ];
       uid = 1000;
       hashedPassword =
         "$6$rBfbbdF/ghJdJo$cn/Hhzve2Lx5xmQR3p81mM.oBZ3PSyDaiUR1CfNZdBn839EFbQWqbLD73tnQCOag8ruDTgxvmwEFMTavwTC.r.";
+    };
+  };
+
+  home-manager.users.me = { ... }: {
+
+    imports = [ packagePath ];
+    config = {
+
+      inherit _repoRoot _monoFont;
+      _secret = ../../_secret;
+
+      nixpkgs.config = {
+        #allowBroken = true;
+        allowUnfree = true;
+        #allowUnsupportedSystem = true;
+        #oraclejdk.accept_license = true;
+      };
+
+      nixpkgs.overlays = builtins.attrValues (import overlayPath);
+      #_module.args.setEnvironment = config.system.build.setEnvironment;
+      home.stateVersion = config.system.stateVersion;
+    };
+
+    # Global options
+    options = with lib; {
+      _repoRoot = mkOption { type = types.path; };
+      _monoFont = mkOption { type = types.attrs; };
+      _secret = mkOption { type = types.path; };
     };
   };
 }
