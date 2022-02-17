8a604a95c4e4408e881b90ab1e12e1ff31113a31
ja0nz <mail@ja.nz>
2022-02-17 14:20:53 +0100

    refactor: improved audio port switcher

--- _home-manager/waybar/switch-audio-port.sh
+++ _home-manager/waybar/switch-audio-port.sh
@@ -2,22 +2,35 @@
 # Requires:
 # - alsa-utils -> amixer
 # - pulseaudio -> pactl
+# - ripgrep, awk
 
 # Ensure Auto-Mute is off
 amixer -c 0 sset 'Auto-Mute Mode' Disabled --quiet
 
-# Get default sink id
-SINK_ID=$(pactl info | sed -En 's/Default Sink: (.*)/\1/p')
+# GET default sink id
+DEFAULT_S_ID=$(pactl info | rg 'Default Sink' | awk '{ print $3 }')
+# GET running sink id
+S_ID=$(pactl list sinks short | awk '{ if ($7=="RUNNING") print $2 }')
+S_ID=$(if [ -z ${S_ID-set} ]; then echo $DEFAULT_S_ID; else echo $S_ID; fi)
 
-# Some port identifiers
-# Hint: 
-# - pactl info -> get the default sink
-# - pactl list -> get Port identifier
-PORT_HEADPHONES="Headphones"
-PORT_SPEAKER="Speaker"
+# GET Available Ports & Active Port
+S_NO=$(pactl list sinks short | awk -v src=$S_ID '{ if ($2==src) print $1 }')
+SINK=$(pactl list sinks | rg -U "^Sink #$S_NO((.|\n)*?)^\s*\$")
+PORTS=$(echo $SINK | rg -o --pcre2 '(?<=Ports:)(.*)(?=Active Port:)')
+A_PORT=$(echo $SINK | rg -o --pcre2 '(?<=Active Port:)(.*)(?=Formats:)' | xargs)
 
-if $(pactl list sinks | grep "Active Port" | grep -q $PORT_SPEAKER); then
-	pactl set-sink-port $SINK_ID "[Out] $PORT_HEADPHONES"
-else
-	pactl set-sink-port $SINK_ID "[Out] $PORT_SPEAKER"
-fi
+# GET an array of port options
+readarray -t PORTS <<<$(echo $PORTS | rg -o '\[Out\]( |\w)+')
+
+# GET an index of current port selection
+for IDX in "${!PORTS[@]}"; do
+   [[ "${PORTS[$i]}" = "${A_PORT}" ]] && break
+done
+
+# Increment index
+IDX=$((IDX + 1))
+
+PORTS_LEN=$(echo ${#PORTS[@]})
+IDX=$(if ((IDX < PORTS_LEN)); then echo $IDX; else echo 0; fi)
+
+pactl set-sink-port "$S_ID" "${PORTS[IDX]}";
