7994dcfc43dd4b081184c0a733fdcee3bdb44393
ja0nz <mail@ja.nz>
2022-02-22 11:06:44 +0100

    chore: use grep instead rg to ensure compat

--- _home-manager/waybar/switch-audio-port.sh
+++ _home-manager/waybar/switch-audio-port.sh
@@ -2,25 +2,24 @@
 # Requires:
 # - alsa-utils -> amixer
 # - pulseaudio -> pactl
-# - ripgrep, awk
 
 # Ensure Auto-Mute is off
 amixer -c 0 sset 'Auto-Mute Mode' Disabled --quiet
 
 # GET default sink id
-DEFAULT_S_ID=$(pactl info | rg 'Default Sink' | awk '{ print $3 }')
+DEFAULT_S_ID=$(pactl info | grep 'Default Sink' | awk '{ print $3 }')
 # GET running sink id
 S_ID=$(pactl list sinks short | awk '{ if ($7=="RUNNING") print $2 }')
 S_ID=$(if [ -z ${S_ID-set} ]; then echo $DEFAULT_S_ID; else echo $S_ID; fi)
 
 # GET Available Ports & Active Port
 S_NO=$(pactl list sinks short | awk -v src=$S_ID '{ if ($2==src) print $1 }')
-SINK=$(pactl list sinks | rg -U "^Sink #$S_NO((.|\n)*?)^\s*\$")
-PORTS=$(echo $SINK | rg -o --pcre2 '(?<=Ports:)(.*)(?=Active Port:)')
-A_PORT=$(echo $SINK | rg -o --pcre2 '(?<=Active Port:)(.*)(?=Formats:)' | xargs)
+SINK=$(pactl list sinks | awk "/^Sink #$S_NO/,/^\s*\$/")
+PORTS=$(echo $SINK | grep -o -P '(?<=Ports:)(.*)(?=Active Port:)')
+A_PORT=$(echo $SINK | grep -o -P '(?<=Active Port:)(.*)(?=Formats:)' | xargs)
 
 # GET an array of port options
-readarray -t PORTS <<<$(echo $PORTS | rg -o '\[Out\]( |\w)+')
+readarray -t PORTS <<<$(echo $PORTS | grep -o -P '\[Out\]( |\w)+')
 
 # GET an index of current port selection
 for IDX in "${!PORTS[@]}"; do
