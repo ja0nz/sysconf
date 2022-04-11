e4690c3681de67b1e1a58236d9103b0b20c5580c
ja0nz <mail@ja.nz>
2022-04-11 09:57:26 +0200

    refactor: comment out discordpy

--- nixOS/ThinkPad_X1_Nano_Gen1/overlays.nix
+++ nixOS/ThinkPad_X1_Nano_Gen1/overlays.nix
@@ -13,20 +13,21 @@
 */
 
 {
-  discordpyOverlay = self: super: {
-    python37 = super.python37.override {
-      packageOverrides = pself: psuper: {
-        discordpy = psuper.discordpy.overrideAttrs (attrs: {
-          patchPhase = ''
-            substituteInPlace "requirements.txt" \
-              --replace "aiohttp>=3.6.0,<3.7.0" "aiohttp>=3.6.0,<3.8.0" \
-          '';
-        });
-      };
-    };
-  };
+  # TODO Fixed upstream; Leave this here for reference
+  # discordpyOverlay = self: super: {
+  #   python37 = super.python37.override {
+  #     packageOverrides = pself: psuper: {
+  #       discordpy = psuper.discordpy.overrideAttrs (attrs: {
+  #         patchPhase = ''
+  #           substituteInPlace "requirements.txt" \
+  #             --replace "aiohttp>=3.6.0,<3.7.0" "aiohttp>=3.6.0,<3.8.0" \
+  #         '';
+  #       });
+  #     };
+  #   };
+  # };
 
-  swaylock-effectsOverlay = self: super: {
+  swaylockOverlay = self: super: {
     swaylock-effects = super.swaylock-effects.overrideAttrs (attr: {
       src = builtins.fetchTarball {
         url =
@@ -36,13 +37,13 @@
     });
   };
 
-  nixCommunity = (import (builtins.fetchTarball {
+  emacsOverlay = (import (builtins.fetchTarball {
     url =
       "https://github.com/nix-community/emacs-overlay/archive/6bad1a2d7ef2fdb822e2374b2c5bdb37d8edda2d.tar.gz";
     sha256 = "1gh2grdbcnmkgj17l8q4ixql73v5cv8kbz1jmscff09ik9sc48cx";
   }));
 
-  myWaylandOverlay = (import "${
+  waylandOverlay = (import "${
       builtins.fetchTarball {
         url =
           "https://github.com/colemickens/nixpkgs-wayland/archive/14fa0af09e80eccc76a6c82595422920aa574608.tar.gz";
