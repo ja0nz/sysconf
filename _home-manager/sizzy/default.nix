eccc9cbdd3550b8426f413ae51b719f270bddc66
ja0nz <mail@ja.nz>
2022-04-15 22:45:50 +0200

    chore: bumped sizzy

--- _home-manager/sizzy/default.nix
+++ _home-manager/sizzy/default.nix
@@ -8,7 +8,7 @@
 
 let
   pname = "sizzy";
-  version = "57.10.0"; # TODO Adjust version number
+  version = "59.2.0"; # TODO Adjust version number
   name = "${pname}-${version}";
   src = ./. + "/Sizzy-${version}.AppImage";
   appimageContents = appimageTools.extractType2 { inherit name src; };
