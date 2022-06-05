9a1af64eef09f159aab9521b2e57a3d68b8c1515
ja0nz <mail@ja.nz>
2022-06-05 14:42:51 +0200

    disabled virtualization

--- nixOS/_shared/virtualization.nix
+++ nixOS/_shared/virtualization.nix
@@ -7,8 +7,9 @@
 */
 { pkgs, ... }:
 
-{
-  programs.dconf.enable = true;
-  environment.systemPackages = with pkgs; [ virt-manager ];
-  virtualisation.libvirtd.enable = true;
-}
+{}
+# {
+#   programs.dconf.enable = true;
+#   environment.systemPackages = with pkgs; [ virt-manager ];
+#   virtualisation.libvirtd.enable = true;
+# }
