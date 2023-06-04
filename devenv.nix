6b08a0ad04c78dfa603ba3cee2492174029b8d95
ja0nz <mail@ja.nz>
2023-06-04 12:18:37 +0200

    ci: lorri -> devenv

---
+++ devenv.nix
@@ -0,0 +1,29 @@
+{ pkgs, ... }:
+
+{
+  packages = with pkgs; [
+    git
+    git-secret # A bash-tool to store your private data inside a git repository
+    nixfmt # An opinionated formatter for Nix
+    manix # A Fast Documentation Searcher for Nix
+    update-nix-fetchgit # A program to update fetchgit values in Nix expressions
+  ];
+
+  pre-commit.hooks = {
+    # hide modified secrets
+    git-secret-hide = {
+      enable = true;
+      name = "git-secret-hide";
+      description = "hide modified secrets";
+      entry = "${pkgs.git-secret}/bin/git-secret hide -m";
+      pass_filenames = false;
+    };
+
+    # format *.nix
+    nixfmt.enable = true;
+  };
+
+  scripts = { hide.exec = "git secret hide -m"; };
+
+  languages.nix.enable = true;
+}
--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    nixfmt # An opinionated formatter for Nix
-    manix # A Fast Documentation Searcher for Nix
-    haskellPackages.update-nix-fetchgit # A program to update fetchgit values in Nix expressions
-  ];
-}
