54e432ae02764a8c63a196cde8647c8b8c456c0c
ja0nz <mail@ja.nz>
2023-08-14 10:36:52 +0200

    feat: moved git secret to sops because of missing pre-commit-hook

--- devenv.nix
+++ devenv.nix
@@ -3,21 +3,12 @@
 {
   packages = with pkgs; [
     git
-    git-secret # A bash-tool to store your private data inside a git repository
+    sops # Mozilla sops (Secrets OPerationS) is an editor of encrypted files
     manix # A Fast Documentation Searcher for Nix
     update-nix-fetchgit # A program to update fetchgit values in Nix expressions
   ];
 
   pre-commit.hooks = {
-    # hide modified secrets
-    git-secret-hide = {
-      enable = true;
-      name = "git-secret-hide";
-      description = "hide modified secrets";
-      entry = "${pkgs.git-secret}/bin/git-secret hide -m";
-      pass_filenames = false;
-    };
-
     # format *.nix
     nixfmt.enable = true;
   };
