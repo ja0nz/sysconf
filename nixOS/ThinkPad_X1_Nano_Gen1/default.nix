54e5277ee19e00e841bd13070fb7b3e893f16ea5
ja0nz <git@ja.nz>
2025-12-01 00:59:55 +0100

    feat: switched to determined nix

--- nixOS/ThinkPad_X1_Nano_Gen1/default.nix
+++ nixOS/ThinkPad_X1_Nano_Gen1/default.nix
@@ -1,11 +1,13 @@
 let
   sources = import ../../npins;
   nixosSystem = import "${sources.nixpkgs}/nixos/lib/eval-config.nix";
+  determinate = (import sources.flake-compat { src = sources.determinate; }).outputs;
 in
 nixosSystem {
   system = null;
   specialArgs = { inherit sources; };
   modules = [
     ./configuration.nix
+    determinate.nixosModules.default
   ];
 }
