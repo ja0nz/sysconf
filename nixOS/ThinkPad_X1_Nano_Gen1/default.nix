2819673d484d140bf1e6dd34a9481d62dd623cb7
ja0nz <git@ja.nz>
2026-03-27 00:30:51 +0100

    feat: added microvm-nix and nix-index-db

--- nixOS/ThinkPad_X1_Nano_Gen1/default.nix
+++ nixOS/ThinkPad_X1_Nano_Gen1/default.nix
@@ -2,6 +2,8 @@ let
   sources = import ../../npins;
   nixosSystem = import "${sources.nixpkgs}/nixos/lib/eval-config.nix";
   determinate = (import sources.flake-compat { src = sources.determinate; }).outputs;
+  nix-index-db = (import sources.flake-compat { src = sources.nix-index-db; }).outputs;
+  microvm-nix = (import sources.flake-compat { src = sources.microvm-nix; }).outputs;
 in
 nixosSystem {
   system = null;
@@ -9,5 +11,8 @@ nixosSystem {
   modules = [
     ./configuration.nix
     determinate.nixosModules.default
+    nix-index-db.nixosModules.default
+    { programs.nix-index-database.comma.enable = true; }
+    microvm-nix.nixosModules.host
   ];
 }
